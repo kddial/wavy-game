249c88165c106274dd4305e3d86295b6a0b66dcf
Kevin Dial <kevin.dial.phone@gmail.com>
2016-03-05 22:02:14 -0500

    tweaked dimensions

--- js/core.js
+++ js/core.js
@@ -24,19 +24,21 @@ var resizeCanvas = function() {
   var width;
   var height;
   var x_padding = 20;
-  var y_padding = 110;
+  var y_padding = 120;
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
   var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
 
+  // when canvas rectangle's width is larger than the height bigger than width
   // if in potrait view, match canvas width to viewport width, leaving white space on top and bottom
   // if in landscape view, match canvas height to viewport height, leaving whitespace on the sides
   if (h >= w) {
-    width = w - x_padding;
+    width =  Math.min(canvas.width - x_padding, w - x_padding);
     height = width * canvasRatio;
   } else {
-    height = h - y_padding;
+    height = Math.min(canvas.height - y_padding, h - y_padding);
     width = height / canvasRatio;
   }
+  console.log("width: " + width + ", height: " + height);
   canvas.style.width =  width + "px";
   canvas.style.height = height + "px";
 };
